cce44f9397fb677b6e18dcddc03f90fbf6afe3b7
Caeoon <752317933@qq.com>
2022-10-10 10:00:02 +0800

    first commit

---
+++ assets/environment/index.js
@@ -0,0 +1,24 @@
+export const environments = [
+  {
+    id: '',
+    name: 'None',
+    path: null,
+  },
+  {
+    id: 'neutral', // THREE.RoomEnvironment
+    name: 'Neutral',
+    path: null,
+  },
+  {
+    id: 'venice-sunset',
+    name: 'Venice Sunset',
+    path: 'assets/environment/venice_sunset_1k.hdr',
+    format: '.hdr'
+  },
+  {
+    id: 'footprint-court',
+    name: 'Footprint Court (HDR Labs)',
+    path: 'assets/environment/footprint_court_2k.hdr',
+    format: '.hdr'
+  }
+];
